0c2843b26ecc6f2960aeafb4f8484cf1457052f5
krishnabansal454 <122722493+krishnabansal454@users.noreply.github.com>
2024-10-16 08:11:57 +0530

    14th oct 2024

--- .idea/javascipt_class/script.js
+++ .idea/javascipt_class/script.js
@@ -1,10 +1,17 @@
 // 
-let a =10;
+// let a =10;
 
-if(a==10&&a<=20&&!a){ // compare only digits of we ==  double equals
-    console.log("true");
-}
-else{
-    console.log("false")
-}
+// if(a==10&&a<=20&&!a){ // compare only digits of we ==  double equals
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+// let a = prompt("Please enter a number:");
+// let result = (isNaN(a)) ? "Not a number" : (a < 0 ? "Negative" : (a>0?"Positive":"Zero"));
+// console.log(result)
 
+// no is random or not 
+const arr = [3, "4", 5, "6", 7, "8", 9, 10, 11, "12"];
+let r = Math.floor(Math.random() * arr.length);
+console.log(typeof(arr[r]));
